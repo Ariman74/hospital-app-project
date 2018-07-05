82e7d91e5dd966f523cc44a4ccd511dfe284cf6d
HomaunW <40868259+HomaunW@users.noreply.github.com>
2018-07-06 00:40:18 +0430

    Initial commit

---
+++ hospital/Controllers/SickController.cs
@@ -0,0 +1,90 @@
+﻿using hospital.Models.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using hospital.Models;
+
+
+
+
+namespace hospital.Controllers
+{
+    public class SickController : Controller
+    {
+
+        Database1Entities3 db;
+
+        public SickController()
+        {
+            db = new Database1Entities3();
+            
+        }
+
+
+        public ActionResult Index()
+        {
+            var sicks = db.Sick.ToList();
+            return View(sicks);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.SexId = new SelectList(db.Sex.ToList(), "Id", "SexName");
+            ViewBag.StatusId = new SelectList(db.Status.ToList(), "Id", "StatusName");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Sick sick)
+        {
+            sick.Id =  Guid.NewGuid();
+
+            db.Sick.Add(sick);
+
+            db.SaveChanges();
+
+            return RedirectToAction( "Index" );
+        }
+
+        public ActionResult Edit(Guid Id)
+        {
+
+            Sick sick = db.Sick.Find(Id);
+
+            ViewBag.SexId = new SelectList(db.Sex.ToList(), "Id", "SexName");
+            ViewBag.StatusId = new SelectList(db.Status.ToList(), "Id", "StatusName");
+
+            return View(sick);
+        }
+        [HttpPost]
+        public ActionResult Edit(Sick sick)
+        {
+            db.Entry(sick).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Detail(Guid Id)
+        {
+            Sick entity = db.Sick.Find(Id);
+            return View(entity);
+        }
+       
+        public ActionResult DetailViewBagResult(Guid Id)
+        {
+            Sick entity = db.Sick.Find(Id);
+            ViewBag.SicknessName = db.Visit.Where(x => x.SickId == Id).ToList();
+            //ViewBag.MdicineName = db.Visit.Where(x => x.UserId == Id).ToList();
+            //Visit model = new Visit();
+            //model.Sick entity = db.Sick.Find(Id);
+            //model.visits = db.Visit.Where(x => x.s)
+
+            return View(entity);
+        }
+        
+    }
+}
---
+++ hospital/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using hospital.Models.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using hospital.Models;
+
+namespace hospital.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        Database1Entities3 db;
+
+        public UserController()
+        {
+            db = new Database1Entities3();
+
+        }
+        public ActionResult Index()
+        {
+            var users = db.User.ToList();
+            return View(users);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.TypeId = new SelectList(db.Type.ToList(), "Id", "TypeName");
+            //ViewBag.StatusId = new SelectList(db.Status.ToList(), "Id", "StatusName");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            user.Id = Guid.NewGuid();
+
+            db.User.Add(user);
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(Guid Id)
+        {
+
+            User user = db.User.Find(Id);
+
+            ViewBag.TypeId = new SelectList(db.Type.ToList(), "Id", "TypeName");
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            db.Entry(user).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
